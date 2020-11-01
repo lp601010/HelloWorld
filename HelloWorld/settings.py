50452154857246eaa8282d60835b2d311927ac3f
lp601010 <15972080144@139.com>
2020-11-01 14:51:37 +0800

    201101

--- HelloWorld/settings.py
+++ HelloWorld/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'TestModel',
 ]
 
 MIDDLEWARE = [
@@ -74,9 +75,14 @@ WSGI_APPLICATION = 'HelloWorld.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default':
+    {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'runoob',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'USER': 'root',
+        'PASSWORD': 'admin',
     }
 }
 
--- HelloWorld/urls.py
+++ HelloWorld/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
  
-from . import views
+from . import views,testdb
  
 urlpatterns = [
     path('runoob/', views.runoob),
+    path('testdb/', testdb.testdb),
 ]
