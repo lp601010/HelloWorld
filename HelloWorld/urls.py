f39bf41c66b596b9336a52b3a2f493bf50f79641
lp601010 <15972080144@139.com>
2020-11-01 23:10:43 +0800

    201101

--- HelloWorld/urls.py
+++ HelloWorld/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
- 
-from . import views,testdb
+from django.urls import path,re_path
+# from django.conf.urls import url
+from . import views,testdb,search,search2
  
 urlpatterns = [
     path('runoob/', views.runoob),
     path('testdb/', testdb.testdb),
+    re_path(r'^search-form$', search.search_form),
+    re_path(r'^search$', search.search),
+    re_path(r'^search-post$', search2.search_post),
 ]
